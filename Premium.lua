735353d899e26f529fb55e434f7e4ac34b7b80c5
ZYSUME <joaquins.apostol@gmail.com>
2026-06-14 08:20:08 +0800

    Delete Premium.lua

--- Premium.lua
+++
@@ -1,3 +0,0 @@
--- EliteVault Secure Loader | 1000 Layers
-local l=loadstring,s=string.format,u="https://raw.githubusercontent.com/ZYSUME/EliteVault/refs/heads/main/obfuscated.lua.txt"
-local c="l(game:HttpGet(%q))()"for _=1,1000 do c=s("l(%q)",c)end l(s(c,u))()
